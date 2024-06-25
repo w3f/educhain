f5fd38d678ea98eee0404a429b5f5468546ee77c
Bader Youssef <ibnbassem@gmail.com>
2024-06-25 09:44:40 -0400

    tests passing

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -940,7 +940,7 @@ mod tests {
                 authoring_version: 1,
                 spec_version: 1,
                 impl_version: 0,
-                apis: RUNTIME_API_VERSIONS,
+                apis: apis::RUNTIME_API_VERSIONS,
                 transaction_version: 1,
                 state_version: 1,
             }
